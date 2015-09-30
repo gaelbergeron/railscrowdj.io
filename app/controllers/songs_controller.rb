86312679fbbb7e24eca0a8842a639813a4332ad1
sstern6 <scott.stern06@gmail.com>
2015-09-30 14:55:49 -0700

    Add successful login, signup, and search songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,19 +3,10 @@ class SongsController < ApplicationController
   def index
   end
 
-
   def create
     @params = params[:song][:title]
     @tracks = client.get('/tracks',:q => "#{params[:song][:title]}")
     render 'show'
-    # @tracks.each do |c|
-    #   p c.stream_url
-    # end
-
   end
-
-
-
-
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_many :songs
+
+  has_secure_password
 end
